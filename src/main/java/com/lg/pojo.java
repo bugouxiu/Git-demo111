b55542456b06f34d539d992e62e8a09c03eed110
lg <lbbwws@icloud.com>
2021-07-31 12:03:07 +0800

    first commit

---
+++ src/main/java/com/lg/pojo.java
@@ -0,0 +1,11 @@
+package com.lg;
+
+/**
+ * @author bugouxiu
+ * @date:2021-07-31 12:01
+ */
+public class pojo {
+    public static void main(String[] args) {
+        System.out.println("hello lg");
+    }
+}
